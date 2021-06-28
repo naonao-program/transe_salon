31a874a28efd808ba3c448f994b2e67f6d15f634
naonao-program <gf19a012@oecu.jp>
2021-06-28 17:15:53 +0900

    userに関してのストロングパラメーターとモデルのバリデーションを記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+#新規登録時のストロングパラメータに「カラム」の追加
+def configure_permitted_parameters
+    #新規登録時のストロングパラメータに「first_nameカラムlast_nameカラムserviseカラム」の追加
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :servise])
 end
